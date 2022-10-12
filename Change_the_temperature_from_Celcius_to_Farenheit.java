aedaddc1c8d6239ac6f5a67d3c1257f5cc2e6853
Dipanwita Das <110822202+dipanwita1405@users.noreply.github.com>
2022-10-12 21:21:00 +0530

    hacktoberfast

---
+++ Change_the_temperature_from_Celcius_to_Farenheit.java
@@ -0,0 +1,13 @@
+public class Change_the_temperature_from_Celcius_to_Farenheit {
+    public static void main(String[] args) 
+    { 
+        // initialising 
+        double celsius = 10.0, fahrenheit = 0.0; 
+        
+        // formula for conversion 
+        fahrenheit = (celsius * 1.8) + 32; 
+        System.out.println( 
+            " value of temperature in fahrenheit:"
+            + fahrenheit); 
+    } 
+}
